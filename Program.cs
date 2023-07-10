fa82cd96adb4cf7bfdbf05615bcdf3dba6989f53
LucasTavaresA <tavares.lassuncao@gmail.com>
2023-07-09 23:59:15 -0300

    Hello, world!

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Licensed under the GPL3 license.
+// See the LICENSE file in the project root for more information.
+
+using Raylib_cs;
+
+namespace Calculator;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.WHITE);
+
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
